bd6d7df080f0809998f834582498921efd184bca
WKHuang <91825822+WKHunag@users.noreply.github.com>
2021-11-25 14:37:40 +0800

    Update periodgenerator.py

--- periodgenerator.py
+++ periodgenerator.py
@@ -4,106 +4,48 @@ Created on Mon Aug 16 13:21:19 2021
 
 @author: DELL_04
 """
-
-def lightDetector(CSVdata):
-  redLED = CSVdata.iloc[:,0]
-  greenLED = CSVdata.iloc[:,1]
-  react2red = [
-                  i
-                  for i in redLED.index
-                  if redLED[i] == 20
-                  ]
-  react2grn = [
-                  i
-                  for i in greenLED.index
-                  if greenLED[i] == 20
-                  ]
-  return react2red, react2grn
-
-def perioddector(CSVdata):
-  redreact, grnreact = lightDetector(CSVdata)
-  for i in range(len(redreact)-1,-1,-1):
-    while redreact[i]-redreact[i-1] > 100:
-      start = redreact[i-1]
-      diff = redreact[i]-redreact[0]
-      if diff > 10000:
-        diff = diff/2
-      break
-  redperiod = [
-            [
-                     i
-                     for i in redreact
-                     if i <= start + diff * j
-                     if CSVdata.iloc[:,0][i] == 20
-                     ]
-            for j in range(0,31)
-            ]
-  grnperiod = [
-               [
-                i
-                for i in grnreact
-                if i <= grnreact[0] + diff * j
-                if CSVdata.iloc[:,1][i] == 20
-                ]
-               for j in range(0,31)
-               ]
-  return redperiod, grnperiod
-
-def modifyperiod(redperiod, grnperiod):
-  for i in np.arange(30,-1,-1):
-    if i <= 0:
-      break
-    else:
-      del redperiod[i][0:len(redperiod[i-1])]
-      del grnperiod[i][0:len(grnperiod[i-1])]
-  while [] in redperiod:
-    redperiod.remove([])
-  while [] in grnperiod:
-    grnperiod.remove([])
-  return redperiod, grnperiod
-
-def removeErr(redperiod,grnperiod):
-  for i in range(0,len(redperiod)):
-    for j in range(0,len(redperiod[i])-2):
-      if redperiod[i][j+1] - redperiod[i][j] != 1:
-        redperiod[i-1] = redperiod[i-1] + redperiod[i][:j+1]
-        del redperiod[i][0:j+1]
-        break
-  for i in range(0,len(grnperiod)):
-    for j in range(0,len(grnperiod[i])-2):
-      if grnperiod[i][j+1] - grnperiod[i][j] != 1:
-        grnperiod[i-1] = grnperiod[i-1] + grnperiod[i][:j+1]
-        del grnperiod[i][0:j+1]
-        break        
-  for i in range(len(grnperiod)-1,-1,-1):
-    if grnperiod[i][0] - grnperiod[i-1][len(grnperiod[i-1])-1] == 1:
-      grnperiod[i-1] = grnperiod[i-1] + grnperiod[i]
-      del grnperiod[i]
-  redlightLength = [
-                  len(redperiod[i])
-                  for i in range(0,len(redperiod))
-                  ]
-  for i in range(len(redlightLength)-1,-1,-1):
-    if redlightLength[i] < 300 and redlightLength[i-1] < 300:
-      redperiod[i-1] = redperiod[i-1] + redperiod[i]
-      del redperiod[i]
-    elif redperiod[i][0] - redperiod[i-1][-1] == 1:
-      redperiod[i-1] = redperiod[i-1] + redperiod[i]
-      del redperiod[i]
-  return redperiod, grnperiod
+import numpy as np
+import pandas as pd
 
 def periodgenerator(CSVdata):
-  redperiod, grnperiod = perioddector(CSVdata)
-  redperiod, grnperiod = modifyperiod(redperiod, grnperiod)
-  for i in range(0,len(redperiod)):
-      if len(redperiod[i]) < 300:
-          redperiod, a = removeErr(redperiod, grnperiod)
-          break
-  for j in range(0,len(grnperiod)):
-      if len(grnperiod[j]) < 300:
-          b, grnperiod = removeErr(redperiod, grnperiod)
-          break
-  timingdf = pd.concat([pd.DataFrame({'redLED':redperiod}),pd.DataFrame({'greenLED':grnperiod})],axis=1)
+  df1 = CSVdata.iloc[:,0]
+  df2 = CSVdata.iloc[:,1]
+  list1 = list(np.where(df1 == 1)[0])
+  list2 = list(np.where(df2 == 1)[0])
+  redLED = []
+  boo1 = True
+  i = 0
+  j = 0
+  while boo1:
+    i += 1
+    if list1[i]-list1[i-1] != 1:
+        redLED.insert(j,list1[:i])
+        j += 1
+        del list1[:i]
+        i = 0
+    elif len(list1) <= 300: 
+        j += 1
+        redLED.insert(j,list1)
+        del list1
+        boo1 = False
+  grnLED = []
+  boo2 = True
+  x = 0
+  y = 0
+  while boo2:
+    x += 1
+    if list2[x]-list2[x-1] != 1:
+        grnLED.insert(y,list2[:x])
+        y += 1
+        del list2[:x]
+        x = 0
+    elif len(list2) <= 1000: 
+        y += 1
+        grnLED.insert(y,list2)
+        del list2
+        boo2 = False        
+        
+  timingdf = pd.concat([pd.DataFrame({'redLED':redLED}),pd.DataFrame({'greenLED':grnLED})],axis=1)
   return timingdf
 
 Daynus = ['Day1','Day2','Day3','Day4','Day5','Testing']
